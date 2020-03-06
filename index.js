d00b173514474d6d2f65a91368ddeb0f11a6ebf5
aliyasser20 <aliyasser20@hotmail.com>
2020-03-06 09:57:13 -0500

    completed

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let result = 0;
+
+  array.forEach(el => {
+    if (Array.isArray(el)) {
+      result += sumItems(el);
+    } else {
+      result += el;
+    }
+  });
+  
+  return result;
+};
+
+const numbersArray = [[1, 2, [[3], 4]], 5, []];
+const numbersArray2 = [[[[[[[[[[[[[1]]]]]]]]]]]]];
+
+console.log(sumItems(numbersArray));
+console.log(sumItems(numbersArray2));
 
 module.exports = sumItems;
